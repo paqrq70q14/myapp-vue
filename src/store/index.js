d13774d9affeffe61af5f6c6f6814ddf8382d1e1
patxing <476522933@qq.com>
2019-04-25 20:33:06 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import state from './state'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 
 
 export default new Vuex.Store({
     //state:存放公用数据
-    state:{
-        city:'北京'
-    },
-    actions: {
-        changeCity(ctx, city) {
-            ctx.commit('changeCity', city)
-        }
-    },
-    mutations: {
-        changeCity(state, city) {
-            state.city = city
-        }
-    }
+    state,
+    mutations
 })
